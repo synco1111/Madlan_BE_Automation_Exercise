6c931e7a8b42d0b87448a6672875a5b48cec08d0
dtalmor1111 <teslacon369@gmail.com>
2022-03-07 14:56:54 +0200

    add another test for validate missing fields

--- tests/test__university_api.py
+++ tests/test__university_api.py
@@ -1,9 +1,9 @@
 import universe_api
 country_list = []
+total_universities_from_db = universe_api.get_university_from_db()
 
 def test_count_matching_univiersities_by_country():
     country_list = ['Israel', 'United States']
-    total_universities_from_db = universe_api.get_university_from_db()
     for country in country_list:
         total_universities_from_api = len(universe_api.get_universities_by_country(country)[0])
         db_count_filtered_univiersities = len(list(filter(lambda item: item['country'] == country ,total_universities_from_db)))
@@ -14,3 +14,9 @@ def test_validate_status_code():
     for country in country_list:
         status_code = universe_api.get_universities_by_country(country)[1]
         assert status_code == 200, f"Exepcted status code to be 200 for country {country}"
+        
+def test_verify_no_missing_fields():
+    fields = ['country', 'id', 'name']
+    for field in fields:
+        expected_missing_fields_count = len(list(filter(lambda item: field not in item ,total_universities_from_db)))
+        assert expected_missing_fields_count == 0, f"Found missing field - {field}"
